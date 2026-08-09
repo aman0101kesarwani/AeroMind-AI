3f536b9cb182b3afc8d149a4c00e6bdf981da05c
Aman0101kesarwani <amankesarwa0101@gmail.com>
2026-08-10 03:00:54 +0530

    Add pdf upload and storage

---
+++ services/pdf_reader.py
@@ -0,0 +1,48 @@
+import fitz        # provided by PyMuPDF
+from pathlib import Path        #Path is used for handling file paths in a clean and platform-independent way.
+
+def extract_pdf_from_pdf(pdf_path : Path):
+    """
+    Extract text from a PDF page by page.
+    """
+
+    document = fitz.open(pdf_path)
+
+    pages=[]
+
+    for page_number, page in enumerate(document):
+
+        page.append({
+            "page" : page_number + 1,
+            "text" : page.get_text()
+        })
+
+    document.close()
+
+    return pages
+
+
+"""
+pdf_path : which is the location of the PDF.
+"""
+
+"""
+the function return :
+[
+    {
+        "page": 1,
+        "text": "Introduction to Aircraft..."
+    },
+    {
+        "page": 2,
+        "text": "Engine Maintenance..."
+    },
+    {
+        "page": 3,
+        "text": "Safety Procedures..."
+    }
+]
+
+
+notice :we keep the page number.
+"""
